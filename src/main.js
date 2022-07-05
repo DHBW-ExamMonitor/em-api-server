b2dcb6f6a7049dd21dae88dd73e70e8a335dea3d
Kevin Ludwig <mail@kevinludwig.com>
2022-07-05 11:46:04 +0200

    Add some changes

--- src/main.js
+++ src/main.js
@@ -7,8 +7,14 @@ const app = express();
 app.use(express.json());
 
 app.get("/kurse", async (req, res) => {
+  console.log("get kurse");
   const kurse = await prisma.kurs.findMany();
   res.json(kurse);
+});
+
+app.post("/kurse", async (req, res) => {
+  console.log("posted to kurse");
+  res.json({ message: "posted to kurse" });
 });
 
 app.listen(process.env.PORT, () =>
