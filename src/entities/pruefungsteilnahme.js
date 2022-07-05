848856ac789221a6785c208010e1f2db1115aeb8
SvenSchaefer <sven.a.schaefer@accenture.com>
2022-07-05 12:09:23 +0200

    finished endpoint notes

---
+++ src/entities/pruefungsteilnahme.js
@@ -0,0 +1,7 @@
+//getpruefungsteilnahme(id) + ModulJoin
+//getpruefungsteilnahmen
+//getpruefungsteilnahmeebystudent(studentid)
+//getstudentbyversuch(studentid, (1,2,3))
+//createpruefungsteilnahme(id)
+//updatepruefungsteilnahme(id, name)
+//deletepruefungsteilnahme(id)
