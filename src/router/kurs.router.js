02e72698cea444ed79fd41810123bebf041a13de
Kevin Ludwig <mail@kevinludwig.com>
2022-08-15 16:26:00 +0200

    Fix some issues

--- src/router/kurs.router.js
+++ src/router/kurs.router.js
@@ -42,6 +42,7 @@ kursRouter.post("/", async (req, res) => {
     const kurs = await prisma.kurs.create({
       data: {
         name: req.body.name,
+        jahrgang: req.body.jahrgang,
       },
     });
     res.json(kurs);
